660f49e36e43862d1562b71ea1a888c9787ceb6b
Stephen Witkowski <stephen.witkowski@protonmail.com>
2023-08-18 22:23:25 -0400

    Adding updated rosgpt

--- src/sjtu_drone/rosgpt/rosgpt/rosgpt.py
+++ src/sjtu_drone/rosgpt/rosgpt/rosgpt.py
@@ -1,28 +1,25 @@
-#!/usr/bin/env python3
-# This file is part of rosgpt package.
-#
-# Copyright (c) 2023 Anis Koubaa.
-# All rights reserved.
-#
-# This work is licensed under the terms of the Creative Commons Attribution-NonCommercial-ShareAlike 4.0
-# International Public License. See https://creativecommons.org/licenses/by-nc-sa/4.0/ for details.
-
-import os
 import json
-import openai
-import rclpy
+import os
 import threading
+from enum import Enum
+from pathlib import Path
+from typing import Dict, Optional, Union
+
+import rclpy
+from ament_index_python import get_package_share_directory
+from flask import Flask, request, send_from_directory
+from flask_cors import CORS
+from flask_restful import Api, Resource
+from geometry_msgs.msg import Pose
+from langchain.agents import AgentType, initialize_agent, load_tools
+from langchain.llms import OpenAI
+from langchain.output_parsers import PydanticOutputParser
+from langchain.prompts import PromptTemplate
+from langchain.tools import tool
+from pydantic import BaseModel, Field, validator
+from rclpy.executors import SingleThreadedExecutor
 from rclpy.node import Node
 from std_msgs.msg import String
-from flask import Flask, request, send_from_directory, jsonify
-from flask_restful import Resource, Api
-from flask_cors import CORS
-import pyttsx3  # pip install pyttsx3 #you need to install libespeak1 on Ubuntu # sudo apt-get install libespeak1
-from rclpy.executors import SingleThreadedExecutor
-import subprocess
-from geometry_msgs.msg import Pose
-
-from ament_index_python import get_package_share_directory
 
 # Instantiate a Flask application object with the given name
 app = Flask(__name__)
@@ -33,27 +30,88 @@ CORS(app)
 # Create an API object that wraps the Flask app to handle RESTful requests
 api = Api(app)
 
-#You must add OPENAI_API_KEY as an environment variable
-#In Ubuntu: echo 'export OPENAI_API_KEY=your_api_key' >> ~/.bashrc
-# Get the API key from the environment variable. 
-openai_api_key = os.getenv('OPENAI_API_KEY')
-#print(openai_api_key)
+LLM = OpenAI(temperature=0)
 
-# Now you can use the openai_api_key variable to authenticate with the OpenAI API
+@tool("command_to_json")
+def command_to_json(command: str) -> str:
+    """Converts a single command to a JSON object suitable for a ROS agent. The command must be in English.
 
+    Args:
+        command (str): The command to be converted.
+    
+    Returns:
+        str: The JSON object.
 
-# Initialize a threading lock for synchronizing access to shared resources
-# when multiple threads are involved
-spin_lock = threading.Lock()
+    Examples:
+        >>> command_to_json("Move down for 2 meters at a speed of 0.4 meters per second.")
+        '{"action": "move", "params": {"linear_speed": 0.4, "distance": 2, "direction": "down"}}'
 
-# Initialize the Text-to-Speech (TTS) engine using the pyttsx3 library
-# you need to install the following dependencies 
-#       sudo apt-get install libespeak1
-#       pip3 install pyttsx3
-tts_engine = pyttsx3.init()
+        >>> command_to_json("Move left for 3 meters at a speed of 0.6 meters per second.")
+        '{"action": "move", "params": {"linear_speed": 0.6, "distance": 3, "direction": "left"}}'
 
-# Create a separate threading lock for synchronizing access to the TTS engine
-tts_lock = threading.Lock()
+        >>> command_to_json("Land the drone.")
+        '{"action": "land", "params": {}}'
+
+        >>> command_to_json("Stop.")
+        '{"action": "stop", "params": {}}'
+    """
+    class ActionType(str, Enum):
+        land = "land"
+        takeoff = "takeoff"
+        move = "move"
+        stop = "stop"
+
+    class MoveParams(BaseModel):
+        linear_speed: float
+        distance: float = Field(..., ge=-1, le=1)
+        direction: str = Field(..., regex="forward|backward|left|right|up|down")
+
+    class Action(BaseModel):
+        action: ActionType
+        params: Optional[Union[Dict, MoveParams]]
+
+    # Set up a parser + inject instructions into the prompt template.
+    parser = PydanticOutputParser(pydantic_object=Action)
+
+    # Prompt
+    prompt = PromptTemplate(
+        template="Format the command as a JSON object suitable for a ROS agent.\n{command}\n",
+        input_variables=["command"],
+    )
+
+    # Run
+    _input = prompt.format_prompt(command=command)
+    model = OpenAI(temperature=0)
+    output = model(_input.to_string())
+    return parser.parse(output)
+
+@tool("drone_location")
+def get_drone_location():
+    """Gets the current drone location.
+
+    Returns:
+        tuple: The current drone location in the form (x, y, z).
+    """
+    return ROSGPTNode.get_loc()
+
+@tool("poi_location")
+def get_poi_location(point_of_interest: str) -> tuple:
+    """Gets the current point of interest location.
+
+    Returns:
+        tuple: The current point of interest location in the form (x, y, z).
+    """
+    poi_map = {
+        'garden': (0, 0, 0),
+        'greenhouse': (1, 1, 1),
+        'kitchen': (2, 2, 2)
+        }
+    return poi_map[point_of_interest]
+
+def load_agent():
+    tools = [get_drone_location, get_poi_location, command_to_json]
+    agent = initialize_agent(tools, LLM, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
+    return agent
 
 
 
@@ -109,36 +167,7 @@ class ROSGPTNode(Node):
             The latest position data in the form (x, y, z), or None if no position data has been received.
         """
         return self.position
-
-        
-        
-
-
-
-def process_and_publish_chatgpt_response(chatgpt_ros2_node, text_command, chatgpt_response, use_executors=True):
-    """
-    Process the chatbot's response and publish it to the 'voice_cmd' topic.
-
-    Args:
-        chatgpt_ros2_node (ROSGPTNode): The ROS2 node instance.
-        text_command (str): The text command received from the user.
-        chatgpt_response (str): The response from the chatbot.
-        use_executors (bool, optional): Flag to indicate whether to use SingleThreadedExecutor. Defaults to True.
-    """
-    chatgpt_ros2_node.publish_message(chatgpt_response) # Publish the chatbot's response using the ROS2 node
-    # If use_executors flag is True, use SingleThreadedExecutor
-    if use_executors:
-        executor = SingleThreadedExecutor()# Create a new executor for each request 
-        executor.add_node(chatgpt_ros2_node) # Add the node to the executor
-        executor.spin_once()#  Spin the executor once
-        executor.remove_node(chatgpt_ros2_node) # Remove the node from the executor
-    # If use_executors flag is False, use spin_lock to synchronize access
-    else:
-        with spin_lock:
-            rclpy.spin_once(chatgpt_ros2_node)
-
-
-
+    
 class ROSGPTProxy(Resource):
     """
     A class derived from flask_restful.Resource, responsible for handling incoming HTTP POST requests.
@@ -152,109 +181,16 @@ class ROSGPTProxy(Resource):
             chatgpt_ros2_node (ROSGPTNode): The ROS2 node instance.
         """
         self.chatgpt_ros2_node = chatgpt_ros2_node
+        self.agent = load_agent()
 
-    def askGPT(self, text_command):
-        """
-        Send a text command to the GPT-3 model and receive a response.
-        Args:
-            text_command (str): The text command to be sent to the GPT-3 model.
-        Returns:
-            str: The response from the GPT-3 model as a JSON string.
-        """
-        # Create the GPT-3 prompt with example inputs and desired outputs
-
-        prompt = '''
-        Consider the following ontology:
-        {"action": "land", "params": {}}
-        {"action": "takeoff", "params": {}}
-        {"action": "move", "params": {"linear_speed": linear_speed, "distance": distance, "direction": direction}}
-
-        You may get a command in another language, translate it to English and then create the JSON.
-
-        The 'direction' parameter can take values "forward", "backward", "left", "right", "up", "down" to indicate the direction of movement. Here are some examples.
-
-        If speed is not given in the prompt, it is assumed to be 0.5 meters per second.
-        All numerical answers should be in float form.
-
-        Prompt: "Move forward for 1 meter at a speed of 0.5 meters per second."
-        Returns: {"action": "move", "params": {"linear_speed": 0.5, "distance": 1, "direction": "forward"}}
-
-        Prompt: "Move backward for 2 meters at a speed of 0.7 meters per second."
-        Returns: {"action": "move", "params": {"linear_speed": 0.7, "distance": 2, "direction": "backward"}}
-
-        Prompt: "Move left for 3 meters at a speed of 0.6 meters per second."
-        Returns: {"action": "move", "params": {"linear_speed": 0.6, "distance": 3, "direction": "left"}}
-
-        Prompt: "Move right for 4 meters at a speed of 0.5 meters per second."
-        Returns: {"action": "move", "params": {"linear_speed": 0.5, "distance": 4, "direction": "right"}}
-
-        Prompt: "Move up for 1 meter at a speed of 0.3 meters per second."
-        Returns: {"action": "move", "params": {"linear_speed": 0.3, "distance": 1, "direction": "up"}}
-
-        Prompt: "Move down for 2 meters at a speed of 0.4 meters per second."
-        Returns: {"action": "move", "params": {"linear_speed": 0.4, "distance": 2, "direction": "down"}}
-
-        Prompt: "Take off."
-        Returns: {"action": "takeoff", "params": {}}
-
-        Prompt: "Land."
-        Returns: {"action": "land", "params": {}}
-
-        Prompt: "Stop."
-        Returns: {"action": "stop", "params": {}}
-
-        You will be given human language prompts, and you need to return a JSON conformant to the ontology. Any action not in the ontology must be ignored.
-
-        Examples in other languages:
-
-        German:
-        Prompt: "Bewegen Sie sich vorwärts für 1 Meter mit einer Geschwindigkeit von 0,5 Metern pro Sekunde."
-        Returns: {"action": "move", "params": {"linear_speed": 0.5, "distance": 1, "direction": "forward"}}
-
-        Japanese:
-        Prompt: "Mae ni 1 meter, 0.5 meters per second no sokudo de susumu."
-        Returns: {"action": "move", "params": {"linear_speed": 0.5, "distance": 1, "direction": "forward"}}
-
-        Hindi:
-        Prompt: "Aage badho 1 meter, 0.5 meters per second ke liye."
-        Returns: {"action": "move", "params": {"linear_speed": 0.5, "distance": 1, "direction": "forward"}}
-        '''
-
-
-        prompt = prompt+'\nprompt: '+text_command
-        #print(prompt) #for testing
-        
-
-        # Create the message structure for the GPT-3 model
-        messages = [
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": prompt}
-        ]
-
-        # Try to send the request to the GPT-3 model and handle any exceptions
-        try:
-            response = openai.ChatCompletion.create(
-                model="gpt-3.5-turbo",
-                messages=messages,
-            )
-        except openai.error.InvalidRequestError as e:
-            print(f"Error: {e}")
-            return None
-        except Exception as e:
-            print(f"Unexpected error: {e}")
-            return None
-        
-        # Extract the GPT-3 model response from the returned JSON
-        chatgpt_response = response.choices[0].message['content'].strip()
-        #print(chatgpt_response)
-        # Find the start and end indices of the JSON string in the response
-        start_index = chatgpt_response.find('{')
-        end_index = chatgpt_response.rfind('}') + 1
-        # Extract the JSON string from the response
-        json_response_dict = chatgpt_response[start_index:end_index]
-        #print('\n\n\njson_response_dict ',json_response_dict)
-        return json.dumps({'text': chatgpt_response, 'json': json_response_dict})
-
+    # def askGPT(self, command) -> str:
+    #     """
+    #     Send a text command to the LangChain agent and return the response.
+    #     Args:
+    #         command (str): The text command to be sent to the GPT-3 model.
+    #     Returns:
+    #         str: The response from the GPT-3 model as a JSON string.
+    #     """
 
 
     def post(self):
@@ -267,33 +203,24 @@ class ROSGPTProxy(Resource):
         """
 
         text_command = request.form['text_command']
-        print ('[ROSGPT] Command received. ', text_command, '. Asking ChatGPT ...')
-        # Run the speak function on a separate thread
-        #print('text_command:', text_command,'\n')
-        chatgpt_response = self.askGPT(text_command)
-        print ('[ROSGPT] Response received from ChatGPT. \n', str(json.loads(chatgpt_response))[:60], '...')
-        #print('eval(chatgpt_response)', eval(chatgpt_response))
-        # Run the speak function on a separate thread
+        chatgpt_response = self.agent.run(text_command)
+        print(chatgpt_response)
 
         if chatgpt_response is None:
             return {'error': 'An error occurred while processing the request'}
 
-        threading.Thread(target=process_and_publish_chatgpt_response, args=(self.chatgpt_ros2_node, text_command, chatgpt_response, True)).start()
-        #print(json.loads(chatgpt_response))
         return json.loads(chatgpt_response)
-
 
 @app.route('/')
 def index():
-    print(os.path.join(get_package_share_directory('rosgpt'), 'webapp'))
-    return send_from_directory(os.path.join(get_package_share_directory('rosgpt'), 'webapp'), 'index.html')
-
+    webapp_dir = Path(get_package_share_directory('rosgpt')) / 'webapp'
+    return send_from_directory(str(webapp_dir), 'index.html')
 
 def main():
     rclpy.init(args=None)
     chatgpt_ros2_node = ROSGPTNode()
     api.add_resource(ROSGPTProxy, '/rosgpt', resource_class_args=(chatgpt_ros2_node,))
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
     rclpy.shutdown()
 
 if __name__ == '__main__':
