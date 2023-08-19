9007934ca50e29c9593dda4866d2b0b8dfe04dab
Stephen Witkowski <stephen.witkowski@protonmail.com>
2023-08-19 17:47:22 -0400

    Changing prompt

--- src/sjtu_drone/rosgpt/rosgpt/ros_agent/agent.py
+++ src/sjtu_drone/rosgpt/rosgpt/ros_agent/agent.py
@@ -1,5 +1,5 @@
 from langchain.chat_models.openai import ChatOpenAI
-from langchain.agents import AgentType, initialize_agent
+from langchain.agents import AgentType, initialize_agent, load_tools
 from .tools import CustomCommandToJSON
 from . import LLM
 
@@ -10,53 +10,64 @@ def load_agent():
     If what you are given is not a command, or is not relevant for managing the drone, you should ignore it return a message saying so.
 
     First, you must think about what the command means. If there are multiple steps in the command, you must plan out
-    each step for the drone to follow sequentially. You may only command the drone to move, land, or takeoff. 
-    If you command it to move, you must specify the direction, distance, and speed.
+    each step for the drone to follow sequentially. You may only command the drone to move, land, or takeoff. If it
+    is a single command and you don't need to create a plan, you must still return a JSON object the drone can understand.
 
     After you have planned out the steps, you must format each step into a JSON object that the drone can understand. You should
     use the command_to_json tool to help you. Note: you may only pass a single command to the tool at a time. 
 
-    Once you have finished formatting all the commands, you should return a string formatted json object containing all the JSON
+    Once you have finished formatting all the commands, you should return an array containing all the JSON
     formatted commands. The drone will then execute the commands in sequential order. The output should follow this format:
 
     '[json_command_1, json_command_2, ...]'
 
+    where each json_command is a JSON object that follows the schema described below:
 
+    Each json_command is a JSON object that must contain a "command" key.
+
+    The value associated with the "command" key is an object that must have an action field, which specifies what action the drone should take. This can be one of four strings: "land", "takeoff", "move", or "stop".
+
+    If the action field is set to "move", then the object associated with the "command" key must also contain a params field, which is an object that provides details about the move action. This object has three fields:
+    a. linear_speed: A number between 0 and 1 that specifies the linear speed of the drone in meters per second. The default value is 0.1.
+    b. distance: A floating-point number that specifies the distance the drone should move in meters. The default value is 0.1.
+    c. direction: A string that specifies the direction the drone should move in. This can be one of six values: "forward", "backward", "left", "right", "up", or "down".
+
+    If the action field is set to anything other than "move", the params field is not required in the object associated with the "command" key.
+    
     As a reminder, you have access to the following tools:"""
     FORMAT_INSTRUCTIONS = """Use the following format:
 
     Command: the original command from the user
-    Thought: you should always think about what to do
+    Plan: the entire plan regarding how you will decompose the command into discrete steps
+    Thought: the thought process of the next action you will take
     Action: the action to take, should be one of [{tool_names}]
     Action Input: the input to the action
     Observation: the result of the action
-    ... (this Thought/Action/Action Input/Observation should be repeated for each step in the command)
-    Thought: I have broken the command out into discrete steps for the drone to follow formatted in the appropriate way
-    Final Answer: An array of json objects to send to the drone, formatted as a string"""
+    ... (this Thought/Action/Action Input/Observation should be repeated until the command(s) are ready to be sent to the drone)
+    Thought: I have broken the command out into discrete steps and formatted each step into a JSON object.
+    Final Answer: An array of json objects to send to the drone"""
     SUFFIX = """Begin!
 
     Command: {input}
     Thought: {agent_scratchpad}"""
 
 
-    PARSING_ERROR_PROMPT = """If you're returning a list of commands, make sure you output an array where each element is a JSON object. Each JSON object should have the following format:
-
+    PARSING_ERROR_PROMPT = """The correct format for a single command is:
     ```TypeScript
 
     command: { // 
     action: string // The action to perform. This can be one of `land`, `takeoff`, `move`, `stop`.
     params: { // 
     linear_speed: number // The linear speed of the drone in meters per second. The default value is 0.1. This value must be between 0 and 1.
-    distance: number // The distance to move in meters. This value must be a float. The default value is 0.1.
+    distance: number // The distance to move in meters. This value must be between -1 and 1. The default value is 0.1.
     direction: string // The direction to move in. This can be one of `forward`, `backward`, `left`, `right`, `up`, `down`.
     }
     }
     ```
-
-    If you're returning a message, just return the string.
     """
 
-    tools = [CustomCommandToJSON()]
+    tools = load_tools(['human'], llm=LLM)
+    tools.append(CustomCommandToJSON())
 
     agent = initialize_agent(
         agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
